49c6967d277fb75077e5032381a126cd8d0eed44
jhes-foddacer <sansbr27@gmail.com>
2025-06-03 16:12:01 -0300

    o resto do codigo que eu esqueci de dar upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Menu from ''
+import Menu from './componentes/Menu';
+import FormAutor from './paginas/FormAutor';
+import FormCategoria from './paginas/FormCategoria';
+import Home from './paginas/Home';
+import ListaAutor from './paginas/ListaAutor';
+import ListaCategoria from './paginas/ListaCategoria';
 
 function App() {
   return (
     <>
-    <Menu/>
-    <Home/>
-    <ListaCategoria/>
-    <FormCategoria/>
+      <BrowserRouter>
+        <Menu />
+
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/listacategoria' element={<ListaCategoria />} />
+            <Route path='/cadastrocategoria' element={<FormCategoria />} />
+            <Route path='/cadastrocategoria/:id' element={<FormCategoria />} />
+
+            <Route path='/listaautor' element={<ListaAutor />} />
+            <Route path='/cadastroautor' element={<FormAutor />} />
+            <Route path='/cadastroautor/:id' element={<FormAutor />} />
+
+            <Route path='*' element={<Home />} />
+          </Routes>
+        </div>
+
+
+
+
+      </BrowserRouter>
     </>
   );
 }
